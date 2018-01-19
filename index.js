1f6bc0ee6e8ddda5a6f7101a05e3b46e21ac7295
Petr Nevyhoštěný <petr.nevyhosteny@gmail.com>
2018-01-19 11:40:30 +0100

    Allow to pass a regex as the annotation
    This commit implements demanded in #3.

--- index.js
+++ index.js
@@ -6,23 +6,43 @@ var async = require('async');
 var path = require('path');
 
 function getDeps(options, str) {
-    var regexp = new RegExp('\\/\\*[\\s\\S]*@' + options.annotation + '(.*)');
-    var match = str.match(regexp);
-    
-    if (match) {
-        return match[1].split(options.separator).map(function (dep) {
-            return dep.trim();
-        });
+    var pattern;
+    if (options.annotation instanceof RegExp) {
+        pattern = options.annotation;
+    } else {
+        pattern = new RegExp('\\/\\*[\\s\\S]*@' + options.annotation + '(.*)');
     }
-    
-    return [];
+
+    var result = null;
+    var deps = [];
+
+    if (pattern.global) {
+        while ((result = pattern.exec(str)) !== null) {
+            deps = getDepsFromResult(deps, options.separator, result);
+        }
+    } else {
+        if ((result = pattern.exec(str)) !== null) {
+            deps = getDepsFromResult(deps, options.separator, result);
+        }
+    }
+
+    return deps;
+}
+
+function getDepsFromResult(deps, separator, result) {
+    return result[1].split(separator).map(function (dep) {
+        return dep.trim();
+    }).reduce(function (deps, dep) {
+        deps.push(dep);
+        return deps;
+    }, deps);
 }
 
 module.exports = function (options) {
     options = options || {};
     options.annotation = options.annotation || 'requires';
     options.separator = options.separator || ',';
-    
+
     var graph = [],
         files = {},
         hasDep;
@@ -37,38 +57,38 @@ module.exports = function (options) {
             this.emit('error', new PluginError('gulp-deps-order', 'Streaming not supported'));
             return cb();
         }
-        
+
         hasDep = false;
-        
+
         getDeps(options, file.contents.toString()).map(function (dep) {
             return path.join(path.dirname(file.path), dep);
         }).forEach(function (dep) {
             hasDep = true;
             graph.push([file.path, dep]);
         });
-        
+
         if (!hasDep) graph.push([file.path]);
-        
+
         files[file.path] = file;
-        
+
         cb();
     }, function (cb) {
         var ordered;
-        
+
         try {
             ordered = toposort(graph).reverse();
         }
-        
+
         catch (e) {
             this.emit('error', new PluginError('gulp-deps-order', e.toString()));
             return cb();
         }
-        
+
         for (var i = 0; i < ordered.length; i++) {
             if (!files[ordered[i]]) continue;
             this.push(files[ordered[i]]);
         }
-        
+
         cb();
     });
 };
--- test.js
+++ test.js
@@ -8,7 +8,7 @@ it('should sort files by dependencies defined using @requires', function (cb) {
 
     /*
      *   === DEPS ===
-     * 
+     *
      *         e.js        f.js
      *        /    \
      * sub/b.js    d.js
@@ -16,9 +16,9 @@ it('should sort files by dependencies defined using @requires', function (cb) {
      *    |   sub/c.js
      *    \    /
      *     a.js
-     * 
+     *
      */
-    
+
     var depsGraph = {
         'a.js' : ['sub/b.js', 'sub/c.js'],
         'sub/b.js' : ['e.js'],
@@ -27,16 +27,16 @@ it('should sort files by dependencies defined using @requires', function (cb) {
         'e.js' : [],
         'f.js' : []
     };
-    
+
     var files = [];
 
     stream.on('data', function (file) {
         assert(depsGraph[file.relative]);
-        
+
         depsGraph[file.relative].forEach(function (dep) {
             assert(files.indexOf(dep) !== -1);
         })
-        
+
         files.push(file.relative);
     });
 
@@ -50,31 +50,31 @@ it('should sort files by dependencies defined using @requires', function (cb) {
         path: __dirname + '/a.js',
         contents: new Buffer('/* @requires sub/b.js, c.js\n */')
     }));
-    
+
     stream.write(new gutil.File({
         base: __dirname,
         path: __dirname + '/d.js',
         contents: new Buffer('/* @requires e.js\n */')
     }));
-    
+
     stream.write(new gutil.File({
         base: __dirname,
         path: __dirname + '/e.js',
         contents: new Buffer('')
     }));
-    
+
     stream.write(new gutil.File({
         base: __dirname,
         path: __dirname + '/f.js',
         contents: new Buffer('')
     }));
-    
+
     stream.write(new gutil.File({
         base: __dirname,
         path: __dirname + '/sub/b.js',
         contents: new Buffer('/* @requires ../e.js\n */')
     }));
-    
+
     stream.write(new gutil.File({
         base: __dirname,
         path: __dirname + '/sub/c.js',
@@ -84,26 +84,86 @@ it('should sort files by dependencies defined using @requires', function (cb) {
     stream.end();
 });
 
-it('should emit an error if there is a cyclic dependency', function (cb) {
-    var stream = depsOrder();
-    
+it('should sort files by dependencies defined using custom annotation regular expression', function (cb) {
+    var stream = depsOrder({
+        annotation: /\/\/\/\s*<reference\s+path="([^"]+)"\s+\/>/g
+    });
+
     /*
      *   === DEPS ===
-     * 
+     *
+     *  b.js
+     *  /   \
+     *  |   c.js
+     *  \    /
+     *   a.js
+     *
+     */
+
+    var depsGraph = {
+        'a.js' : ['b.js', 'c.js'],
+        'b.js' : [],
+        'c.js' : ['b.js']
+    };
+
+    var files = [];
+
+    stream.on('data', function (file) {
+        assert(depsGraph[file.relative]);
+
+        depsGraph[file.relative].forEach(function (dep) {
+            assert(files.indexOf(dep) !== -1);
+        })
+
+        files.push(file.relative);
+    });
+
+    stream.on('end', function () {
+        assert.equal(files.length, 3);
+        cb();
+    });
+
+    stream.write(new gutil.File({
+        base: __dirname,
+        path: __dirname + '/a.js',
+        contents: new Buffer('/// <reference path="b.js" />\n/// <reference path="c.js" />')
+    }));
+
+    stream.write(new gutil.File({
+        base: __dirname,
+        path: __dirname + '/b.js',
+        contents: new Buffer('')
+    }));
+
+    stream.write(new gutil.File({
+        base: __dirname,
+        path: __dirname + '/c.js',
+        contents: new Buffer('/// <reference path="b.js" />')
+    }));
+
+    stream.end();
+});
+
+it('should emit an error if there is a cyclic dependency', function (cb) {
+    var stream = depsOrder();
+
+    /*
+     *   === DEPS ===
+     *
      *    a.js <-- b.js
      *     |        ^
      *     v        |
      *    d.js --> c.js
-     * 
+     *
      */
-    
+
     var orderedFiles = ['e.js', 'd.js', 'sub/b.js', 'sub/c.js', 'a.js'];
 
     stream.on('error', function (err) {
         assert.notEqual(err, undefined);
         cb();
     });
-    
+
     stream.on('data', function () {
         throw 'Cyclic dependency must emit an error';
         cb();
@@ -114,19 +174,19 @@ it('should emit an error if there is a cyclic dependency', function (cb) {
         path: __dirname + '/a.js',
         contents: new Buffer('/* @requires d.js\n */')
     }));
-    
+
     stream.write(new gutil.File({
         base: __dirname,
         path: __dirname + '/b.js',
         contents: new Buffer('/* @requires a.js\n */')
     }));
-    
+
     stream.write(new gutil.File({
         base: __dirname,
         path: __dirname + '/c.js',
         contents: new Buffer('/* @requires b.js\n */')
     }));
-    
+
     stream.write(new gutil.File({
         base: __dirname,
         path: __dirname + '/d.js',
